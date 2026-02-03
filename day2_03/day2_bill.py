c6db41277da3861683f3188be3ee8c75c89368a1
Spideyabhiii <abhirathod116@gmail.com>
2026-02-03 11:31:15 +0530

    tast 2:bill spliter

---
+++ day2_03/day2_bill.py
@@ -0,0 +1,8 @@
+t_bill=float(input("enter total restuarant bill:"))
+frnd=int(input("enter no. of frnds:"))
+share_per_person=t_bill/frnd
+print("total bill:",t_bill)
+print("each person pays:", share_per_person)
+print("atatype of total_bill:", type(t_bill))
+print("data type of frnds:",type(frnd))
+print("datatype of share per person:", type(share_per_person))
