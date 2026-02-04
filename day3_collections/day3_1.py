b355e9d310919695e9e096e2b62c0f2a30479ce0
Spideyabhiii <abhirathod116@gmail.com>
2026-02-04 11:56:35 +0530

    day3 task1

---
+++ day3_collections/day3_1.py
@@ -0,0 +1,13 @@
+inventory = ["Apples", "Bananas", "Carrots", "Dates"]
+print("Current Inventory:", inventory)
+print("Add Eggs to the inventory")
+inventory.append("Eggs")
+print(inventory)
+print("Remove Bananas from the inventory")
+inventory.remove("Bananas")
+print(inventory)
+print("Sort the inventory alphabetically")
+inventory.sort()
+print(inventory)
+
+print("Final Updated Inventory:", inventory)
